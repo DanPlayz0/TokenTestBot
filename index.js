87bc711169b97b2a29198ad42a5c0479e769b056
DanPlayz <damuffin36@gmail.com>
2021-09-21 18:09:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const id = Buffer.from(process.argv[2].split(".")[0], "base64");
-
+const id = Buffer.from(process.argv[2].split(".")[0], "base64").toString();
 if (id === "mfa") throw "Using user tokens in the API is against ToS.";
-
 console.log("User ID:", id);
